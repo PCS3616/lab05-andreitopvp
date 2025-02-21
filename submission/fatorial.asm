9a041c63e80b6c2552de67b2062edd57eb47d577
andreitopvp <andre.ito@outlook.com>
2025-02-21 15:19:47 -0300

    Add files via upload

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,23 +1,24 @@
-LD N
-SC /200
-JZ /00C
-JP /006
-HM /000
+LD N        # Carrega N
+JZ FIM      # Se N == 0, termina o programa
 
-@ /200
-JP /000
-JZ /20E
-ML RES
-MM RES
-LD N
-SB UM
-MM N
-RS /200
+LD UM       # Carrega a constante 1
+MM RES      # Inicializa RES = 1
 
+@LOOP
+LD N        # Carrega N
+JZ FIM      # Se N == 0, termina o loop
+ML RES      # Multiplica RES por N
+MM RES      # Armazena o novo RES
 
-@ /102
-RES K/1
+LD N        # Carrega N
+SB UM       # Subtrai 1
+MM N        # Atualiza N
+JP LOOP     # Repete enquanto N > 0
 
-@ /220
-UM K/1
+@FIM
+HM /000     # Finaliza o programa
 
+@DADOS
+N   K/5     # Número para calcular fatorial (exemplo: 5)
+RES K/1     # Inicializa RES = 1
+UM  K/1     # Constante 1
