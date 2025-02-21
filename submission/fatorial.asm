629940beece1af3bb7bc2d1b956b9c3e495c26b9
andreitopvp <andre.ito@outlook.com>
2025-02-21 16:02:59 -0300

    Add files via upload

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,22 +1,26 @@
 LD N
 SC /200
 JZ /00C
-JP /006
-HM /000
+JP VERIFICAR
+JP FIM
+
+
+@/230
+FIM HM /000
 
 @ /0200
-JP /000
-JZ /20E
-ML RES
-MM RES
-LD N
-SB UM
-MM N
-RS /200
+VERIFICAR JP /000
+          JZ /20E
+          ML RES
+          MM RES
+          LD N
+          SB UM
+          MM N
+          RS /200
 
 
 @ /0102
-RES K/1
+RES K/0001
 
 @ /0220
 UM K/0001
